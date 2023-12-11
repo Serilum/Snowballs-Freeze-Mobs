7f4f4c61d41bc7a18c4c038e36ee95144962960e
Rick South <rick@natamus.com>
2023-12-12 00:05:02 +0100

    Snowballs Freeze Mobs source code for 1.18.2.

---
+++ Forge/src/main/java/com/natamus/snowballsfreezemobs/forge/events/ForgeSnowEvent.java
@@ -0,0 +1,25 @@
+package com.natamus.snowballsfreezemobs.forge.events;
+
+import com.natamus.snowballsfreezemobs.events.SnowEvent;
+import net.minecraft.world.entity.Entity;
+import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class ForgeSnowEvent {
+	@SubscribeEvent
+	public void onEntityHurt(LivingHurtEvent e) {
+		Entity entity = e.getEntity();
+		if (SnowEvent.onEntityHurt(entity.getCommandSenderWorld(), entity, e.getSource(), e.getAmount()) == 0.0F) {
+			e.setCanceled(true);
+		}
+	}
+	
+	@SubscribeEvent
+	public void onLivingUpdate(LivingEvent.LivingUpdateEvent e) {
+		Entity entity = e.getEntity();
+		SnowEvent.onLivingUpdate(entity.getCommandSenderWorld(), entity);
+	}
+}
