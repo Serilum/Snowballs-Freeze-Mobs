fa2ff06a1f452ac3aeeedb041e68dcb9276b96ae
Rick South <rick@natamus.com>
2024-04-28 18:57:29 +0200

    (v3.4) (1.20.5 Neo) Updated to work with NeoForge's TickEvent changes in 20.5.14.

--- NeoForge/src/main/java/com/natamus/snowballsfreezemobs/neoforge/events/NeoForgeSnowEvent.java
+++ NeoForge/src/main/java/com/natamus/snowballsfreezemobs/neoforge/events/NeoForgeSnowEvent.java
@@ -2,10 +2,11 @@ package com.natamus.snowballsfreezemobs.neoforge.events;
 
 import com.natamus.snowballsfreezemobs.events.SnowEvent;
 import net.minecraft.world.entity.Entity;
-import net.neoforged.neoforge.event.entity.living.LivingEvent;
+import net.minecraft.world.entity.LivingEntity;
 import net.neoforged.neoforge.event.entity.living.LivingHurtEvent;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.tick.EntityTickEvent;
 
 @EventBusSubscriber
 public class NeoForgeSnowEvent {
@@ -18,8 +19,12 @@ public class NeoForgeSnowEvent {
 	}
 	
 	@SubscribeEvent
-	public static void onLivingUpdate(LivingEvent.LivingTickEvent e) {
+	public static void onLivingUpdate(EntityTickEvent e) {
 		Entity entity = e.getEntity();
+		if (!(entity instanceof LivingEntity)) {
+			return;
+		}
+
 		SnowEvent.onLivingUpdate(entity.level(), entity);
 	}
 }
