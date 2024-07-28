951ae53bf804caff4c497c659d91ccee2daabda6
Rick South <rick@natamus.com>
2024-07-28 07:14:35 +0200

    (v3.7) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/snowballsfreezemobs/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/snowballsfreezemobs/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.snowballsfreezemobs;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.snowballsfreezemobs.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.snowballsfreezemobs.neoforge.events.NeoForgeSnowEvent;
 import com.natamus.snowballsfreezemobs.util.Reference;
@@ -14,6 +15,10 @@ import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
