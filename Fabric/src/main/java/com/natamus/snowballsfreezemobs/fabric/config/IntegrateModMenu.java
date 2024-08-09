dd97fd848c020baa25cdd6efd939ea8a1b0a1b96
Rick South <rick@natamus.com>
2024-08-09 08:05:27 +0200

    Snowballs Freeze Mobs source code for 1.21.1.

---
+++ Fabric/src/main/java/com/natamus/snowballsfreezemobs/fabric/config/IntegrateModMenu.java
@@ -0,0 +1,13 @@
+package com.natamus.snowballsfreezemobs.fabric.config;
+
+import com.natamus.collective.config.DuskConfig;
+import com.natamus.snowballsfreezemobs.util.Reference;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+
+public class IntegrateModMenu implements ModMenuApi {
+	@Override
+	public ConfigScreenFactory<?> getModConfigScreenFactory() {
+		return parent -> DuskConfig.DuskConfigScreen.getScreen(parent, Reference.MOD_ID);
+	}
+}
