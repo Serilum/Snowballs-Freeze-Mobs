bb0b44b9da12d5e965c8e185316a73835c9bd349
Rick South <rick@natamus.com>
2026-06-18 22:24:04 +0200

    Updated Snowballs Freeze Mobs to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/snowballsfreezemobs/util/Reference.java
+++ Common/src/main/java/com/natamus/snowballsfreezemobs/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "snowballsfreezemobs";
 	public static final String NAME = "Snowballs Freeze Mobs";
 	public static final String VERSION = "4.0";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
